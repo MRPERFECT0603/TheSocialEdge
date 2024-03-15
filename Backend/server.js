1d3fabe310a07c6fe3978c515aa6cb8d72a645c4
Vivek Shaurya <97908880+MRPERFECT0603@users.noreply.github.com>
2024-03-15 20:10:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 
-app.use(cors({ origin: "https://thesocialedge-api.onrender.com" }));
+app.use(cors({ origin: "the-social-edge-client.vercel.app" }));
 
 app.use(cookieParser());
 
