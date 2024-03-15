7ca653ddcd61824293a0fba05be1dd5c9bb2ef7d
Vivek Shaurya <97908880+MRPERFECT0603@users.noreply.github.com>
2024-03-15 20:12:00 +0530

    Update axios.js

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://thesocialedge-api.onrender.com/api/",
   withCredentials: true,
 });
