277400211f453122a0513c699eba3e5a926e24d4
Naman Gupta <namangupta990@gmail.com>
2024-03-17 12:29:28 +0530

    Update Register.jsx

--- Frontend/src/pages/register/Register.jsx
+++ Frontend/src/pages/register/Register.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Register.scss";
-import axios from "axios";
+import { makeRequest } from "../../axios";
+
 
 const Register = () => {
   const [inputs, setInputs] = useState({
@@ -20,7 +21,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post("https://thesocialedge-api.onrender.com/api/auth/register", inputs);
+      await makeRequest.post("/auth/register", inputs);
     } catch (err) {
       setErr(err.response.data);
     }
