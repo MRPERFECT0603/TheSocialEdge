9361326a174c26128ebbec62a1d48dd726b914eb
Vivek Shaurya <97908880+MRPERFECT0603@users.noreply.github.com>
2024-03-17 12:57:33 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,18 +21,29 @@ app.use(fileUpload());
 
 //middleware
 // In your API routes or middleware
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://the-social-edge-client.vercel.app");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+
+// app.use(express.json());
+
+// // app.use(cors({ origin: "https://the-social-edge-client.vercel.app" }));
+// app.use(cors({ origin: "http://localhost:5173" }));
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://the-social-edge-client.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
 });
-
 app.use(express.json());
-
-// app.use(cors({ origin: "https://the-social-edge-client.vercel.app" }));
-app.use(cors({ origin: "http://localhost:5173" }));
+// app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 
 app.use(cookieParser());
